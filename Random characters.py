3aa11016333b1097ea891ea299fca46fd9f219ab
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-01-21 17:50:37 +0100

    New and/or Updated

--- Random characters.py
+++ Random characters.py
@@ -1,8 +1,8 @@
 """
 This is free and unencumbered software released into the public domain.
 
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
+Anyone is free to copy, modelify, publish, use, compile, sell, or
+distribute this software, either in source code form or as b compiled
 binary, for any purpose, commercial or non-commercial, and by any
 means.
 
@@ -28,94 +28,88 @@ import random
 import string
 
 a = 0
-
-input("To use this file, please run it in the console. Press enter to continue")
-
-
-def no_update():
-    with open("Random characters.txt", "a") as f:
-        print("Use CTRL+C to stop")
-        while True:
-            f.write(random.choice(string.printable))
+b = ""
+c = 1000000
 
 
-def constant_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            f.write(random.choice(string.printable))
-            a = a + 1
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def oneM_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(1000000):
-                f.write(random.choice(string.printable))
-            a = a + 1000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def tenM_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(10000000):
-                f.write(random.choice(string.printable))
-            a = a + 10000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def hundredM_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(100000000):
-                f.write(random.choice(string.printable))
-            a = a + 100000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def oneB_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(1000000000):
-                f.write(random.choice(string.printable))
-            a = a + 1000000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def tenB_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(10000000000):
-                f.write(random.choice(string.printable))
-            a = a + 10000000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def hundredB_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(100000000000):
-                f.write(random.choice(string.printable))
-            a = a + 100000000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def oneT_update():
-    with open("Random characters.txt", "a") as f:
-        while True:
-            for i in range(1000000000000):
-                f.write(random.choice(string.printable))
-            a = a + 1000000000000
-            print("Written a total of", a, "characters, use CTRL+C to stop")
-
-
-def calculations():
-    if a < 1000:
+def ask():
+    global b
+    b = input("How constantly do you want to be updated with? \nAvalible options: \n'ALWAYS' (100%) (slowest)\n "
+              "'USUALLY' (75%)\n 'MOSTLY' (50%)\n 'SOMETIMES' (25%)\n 'RARELY' (10%)\n  'NEVER' (fastest option)\n "
+              "All calculated in 1M ")
+    if b == "ALWAYS":
         pass
-    elif a < 1000000:
+    if b == "USUALLY":
         pass
-    elif a < 1000000000:
+    if b == "MOSTLY":
         pass
-    elif a < 1000000000000:
+    if b == "SOMETIMES":
         pass
+    if b == "RARELY":
+        pass
+    if b == "NEVER":
+        pass
+    else:
+        print("\nUnavalible option, please try again\n")
+        ask()
+    read()
+
+
+def read():
+    global a
+    with open("Random Characters.txt", "a") as q:
+        pass
+    with open("Random Characters.txt", "r") as r:
+        r.read()
+        print(r)
+        print(len(r))
+        if len(r) >= 0:
+            print("No text found, continueing...")
+            write()
+        else:
+            a = len(r)
+            print("There is a total of", len(r), "characters found")
+            write()
+
+
+def write():
+    global a, c
+    with open("Random Characters.txt", "a") as w:
+        if b == "ALWAYS":
+            while True:
+                w.write(random.choice(string.printable))
+                a = a + 1
+                print("There's a total of", a, "characters now")
+        elif b == "USUALLY":
+            while True:
+                for i in range(c / 25):
+                    w.write(random.choice(string.printable))
+                a = a + c / 25
+                print("There's a total of", a, "characters now")
+        elif b == "MOSTLY":
+            while True:
+                for i in range(c / 50):
+                    w.write(random.choice(string.printable))
+                a = a + c / 50
+                print("There's a total of", a, "characters now")
+        elif b == "SOMETIMES":
+            while True:
+                for i in range(c / 75):
+                    w.write(random.choice(string.printable))
+                a = a + c / 75
+                print("There's a total of", a, "characters now")
+        elif b == "RARELY":
+            while True:
+                for i in range(c / 90):
+                    w.write(random.choice(string.printable))
+                a = a + c / 90
+                print("There's a total of", a, "characters now")
+        elif b == "NEVER":
+            while True:
+                w.write(random.choice(string.printable))
+        else:
+            print("Unknown option, please try again")
+            ask()
+
+
+ask()
