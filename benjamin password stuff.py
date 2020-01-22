70d594346d13e8d16b39dcce041e0709496d7f34
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-01-22 12:41:01 +0100

    New and/or Updated

---
+++ benjamin password stuff.py
@@ -0,0 +1,68 @@
+"""
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+"""
+import random
+import time
+
+b = input("What is the password?\n")
+if b == "h":
+    print("The password is 0123456789")
+    exit()
+else:
+    time.sleep(1)
+    print("Connecting to hacking server...")
+    time.sleep(5)
+    print("Receiving data...")
+    time.sleep(3)
+    print("Decoding encrypted data...")
+    time.sleep(10)
+    print("Getting stolen data....")
+    time.sleep(3)
+    print("Displaying data...")
+    time.sleep(3)
+    print("ERROR: unable to display data, dumping data.")
+    time.sleep(0.5)
+    while True:
+        print(random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1))
