8f4690255491ec5aa9b72f4470da2bbc43b32f6a
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-02-01 11:19:37 +0100

    New and/or Updated

--- times 2.py
+++ times 2.py
@@ -30,11 +30,15 @@ c = 1000
 d = 0
 with open("Times 2.txt", "w") as f:
     while True:
-        c = c * 1000
-        print("Calculated a total of", c, "numbers")
-        for i in range(1000):
-            a = a * 2
-            b.append(a)
-        f.write(str(b))
-        f.write("\n")
-        b.clear()
+        try:
+            c = c * 1000
+            print("Calculated a total of", c, "numbers")
+            for i in range(1000):
+                a = a * 2
+                b.append(a)
+            f.write(str(b))
+            f.write("\n")
+            b.clear()
+        except KeyboardInterrupt:
+            print("Calculations canceled")
+            break
