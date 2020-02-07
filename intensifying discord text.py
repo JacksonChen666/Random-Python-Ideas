b82c39b204bbe7af4ad2a847f48b869de47ced18
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-02-07 14:00:39 +0100

    Fixed and more

--- intensifying discord text.py
+++ intensifying discord text.py
@@ -1,14 +1,18 @@
+import os
+
 a = input("Text to overintensify (Discord)\n")
 b = list(a)
 with open("TEMP.txt", "w") as f:
     f.write("***")
     for i in range(len(a)):
-        f.write(a[i])
-        f.write("*")
+        if b[i] == " ":
+            f.write(" ")
+        else:
+            f.write(a[i])
+            f.write("*")
     f.write("**")
 
 with open("TEMP.txt", "r") as w:
     h = w.read()
     print(h)
-    print(
-        "This program is unoptimized for proper formatting on discord. Use the preview feature in the text field instead.")
+    os.remove("TEMP.txt")
