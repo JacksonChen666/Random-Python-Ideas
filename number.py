ee3d350c1df85b6de16674ad168d121d5187e256
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-02-11 15:45:01 +0100

    Update

--- number.py
+++ number.py
@@ -33,18 +33,20 @@ b = 0
 # Adds 1 to blue, then if it is over 255, add 1 to green, then if it goes over again, add 1 to red and stop if over 255
 def main():
     global r, g, b
-    while True:
-        print(r, g, b)
-        b += 1
-        if b >= 255:
-            b = 0
-            g += 1
-        if g >= 255:
-            r += 1
-            g = 0
-        if r >= 255:
-            print("Done")
-            exit()
+    with open("RGB numbers.txt", "w") as n:
+        while True:
+            # print(r, g, b)
+            n.write("{} {} {}\n".format(r, g, b))
+            b += 1
+            if b > 255:
+                b = 0
+                g += 1
+            if g > 255:
+                r += 1
+                g = 0
+            if r > 255:
+                print("Done")
+                exit()
 
 
 while True:
