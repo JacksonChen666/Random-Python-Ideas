6d055e1f5aaeb45563a810c2b4667a86e068905f
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-02-24 17:42:28 +0100

    Stuff

--- turtle random text.py
+++ turtle random text.py
@@ -32,9 +32,10 @@ import turtle
 def __init__():
     global t, q, a, s, d, f
     t = turtle.Pen()
-    q = random.choice(string.printable)
+    s = turtle.Screen()
+    s.tracer(10)
     t.up()
-    t.speed(5)
+    t.speed(0)
     q = 300
     w = 250
     # Main way
@@ -47,28 +48,29 @@ def __init__():
     x = turtle.window_height() / 1.6142
     c = turtle.window_width() / 1.4237288134
     v = turtle.window_width() / 1.4237288134
-    # t.ht()
+    t.ht()
     print(a, s, d, f, z, x, c, v)
-    # ts = turtle.Screen()
-    # ts.setup(width=0.0, height=0.0)
 
 
 def main():
     # noinspection PyGlobalUndefined
     global q, a, s, d, f
-    turtle.bye()
-    t.goto(a, d)
-    t.dot(10)
-    t.goto(s, f)
-    t.dot(10)
-    t.goto(d, a)
-    t.dot(10)
-    t.goto(f, s)
-    t.dot(10)
-
-
-# while True:
-#     t.goto(random.randint(s, a), random.randint(f, d))
+    # t.goto(a, d)
+    # t.dot(10)
+    # t.goto(s, f)
+    # t.dot(10)
+    # t.goto(d, a)
+    # t.dot(10)
+    # t.goto(f, s)
+    # t.dot(10)
+    # turtle.done()
+    while True:
+        for i in range(2500):
+            q = random.choice(string.printable)
+            t.goto(random.randint(s, a), random.randint(f, d))
+            t.write("{}".format(q),
+                    align="center", font=("Arial", random.randint(10, 100), "normal"))
+        t.clear()
 
 
 # Idea: turtle writes random text in random size and color
