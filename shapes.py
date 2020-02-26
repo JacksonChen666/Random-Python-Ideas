05021c435bbf8c4444420e1f0a4bcec8e761a2de
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-02-26 14:29:03 +0100

    Stuff

--- shapes.py
+++ shapes.py
@@ -27,11 +27,25 @@ For more information, please refer to <https://unlicense.org>
 import turtle
 
 t = turtle.Pen()
+s = turtle.Screen()
 a = 2
-t.speed(0)
-
+t.speed(5)
+\
 while True:
     t.write(a, font=("Arial", 50, "normal"), align="center")
+    if a >= 11:
+        t.speed(0)
+        s.tracer(5)
+    if a > 49 < 98:
+        s.tracer(10)
+    if a > 99 < 498:
+        s.tracer(100)
+    if a > 499 < 1000:
+        s.tracer(500)
+    if a > 1001:
+        t.clear()
+        t.write("The limit of sides has reached\nPlease click to exit", font=("Arial", 20, "normal"), align="center")
+        turtle.exitonclick()
     for i in range(a):
         t.forward(500 / a)
         t.left(360 / a)
