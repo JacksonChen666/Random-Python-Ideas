9036b9c755e5f04b9f71de629c6943051056e668
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-04-12 14:47:12 +0200

    sdi

--- randomGenerator.py
+++ randomGenerator.py
@@ -1,79 +1,82 @@
-import random
-import string
+"""
+This Module/Library generates random numbers and strings using the random and string module
+"""
 
 
-# noinspection PyMethodFirstArgAssignment
-class string(amount):
+def setSeed(seed=None):
     """
-    Random string generators
+    Uses the input to set the seed for the random module
+    :param seed: The seed for the random module (Default is random)
+    :return:
     """
-
-    def randomString(self):
-        """
-        Generates random string from string.printable (Includes new lines etc.)
-        :param self: Amount of characters to generate (Default 10)
-        :return:
-        """
-        temp = []
-        if self is None:
-            self = 10
-        for i in range(self):
-            temp.append(random.choice(string.printable))
-
-    def randomLowercase(self):
-        """
-        Generates random lowercase character(s)
-        :param self: Amount of characters to generate (Default 10)
-        :return:
-        """
-        temp = []
-        if self is None:
-            self = 10
-        for i in range(self):
-            temp.append(random.choice(string.lowercase))
+    if seed is None:
+        random.seed(random.randint(0,999999999))
+    else:
+        random.seed(seed)
 
 
-# noinspection PyMethodFirstArgAssignment
-class interger(amount, min, max):
+def randomString(amount):
     """
-    Random number generators
+    Generates random string from string.printable (Includes new lines etc.)
+    :param amount: Amount of characters to generate (Default 10)
+    :return:
     """
+    temp = []
+    if amount is None:
+        amount = 10
+    for i in range(amount):
+        temp.append(random.choice(string.printable))
 
-    def randomFullNumber(self, min, max):
-        """
-        Generates random number(s) without decimal points
-        :param min: Minimum number to generate
-        :param max: Maximum number to generate
-        :param self: Amount of numbers to generate (Default 1)
-        :return:
-        """
-        temp = []
-        if min is None:
-            min = 0
-        if max is None:
-            raise randomGenerator.MissingArgument("Missing argument: Maximum number")
-        if self is None:
-            self = 1
-        for i in range(self):
-            temp.append(random.randint(min, max))
 
-    def randomDecimalNumber(self, min, max):
-        """
-        Generates number(s) with decimal points
-        :param min: Minimum number to generate
-        :param max: Maximum number to generate
-        :param self: Amount of numbers to generate (Default 1)
-        :return:
-        """
-        temp = []
-        if min is None:
-            min = 0
-        if max is None:
-            raise randomGenerator.MissingArgument("Missing argument: Maximum number")
-        if self is None:
-            self = 1
-        for i in range(self):
-            temp.append(random.randint(min, max))
+def randomLowercase(amount):
+    """
+    Generates random lowercase character(s)
+    :param amount: Amount of characters to generate (Default 10)
+    :return:
+    """
+    temp = []
+    if amount is None:
+        amount = 10
+    for i in range(amount):
+        temp.append(random.choice(string.lowercase))
+
+
+def randomFullNumber(amount, min, max):
+    """
+    Generates random number(s) without decimal points
+    :param min: Minimum number to generate
+    :param max: Maximum number to generate
+    :param amount: Amount of numbers to generate (Default 1)
+    :return:
+    """
+    temp = []
+    if min is None:
+        min = 0
+    if max is None:
+        raise randomGenerator.MissingArgument("Missing argument: Maximum number")
+    if amount is None:
+        amount = 1
+    for i in range(amount):
+        temp.append(random.randint(min, max))
+
+
+def randomDecimalNumber(amount, min, max):
+    """
+    Generates number(s) with decimal points
+    :param min: Minimum number to generate
+    :param max: Maximum number to generate
+    :param amount: Amount of numbers to generate (Default 1)
+    :return:
+    """
+    temp = []
+    if min is None:
+        min = 0
+    if max is None:
+        raise randomGenerator.MissingArgument("Missing argument: Maximum number")
+    if amount is None:
+        amount = 1
+    for i in range(amount):
+        temp.append(random.randint(min, max))
 
 
 def __init__():
